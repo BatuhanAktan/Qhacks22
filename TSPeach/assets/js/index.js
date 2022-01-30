6c32bf735c4da197cfd57fffac6bd083c51e3fe5
Matt Sharpe <matthew0sharpe@gmail.com>
2022-01-30 06:51:07 -0500

    Final Without comments

--- TSPeach/assets/js/index.js
+++ TSPeach/assets/js/index.js
@@ -21,8 +21,7 @@ const run = async () => {
 
   if (isRecording) { 
     buttonEl.innerText="Begin Reading";
-    caption_div.style.width = '100%';
-    sen_box.innerHTML="(Press Start And Begin Reading)";
+    
     if (socket) {
       socket.send(JSON.stringify({terminate_session: true}));
       socket.close();
@@ -35,6 +34,8 @@ const run = async () => {
     }
   } else {
     sen_box.innerHTML = "";
+    caption_div.style.width = 'fit-content';
+    messageEl.innerText="Feedback Will Appear Here";
     buttonEl.innerText="Waiting for Assembly AI";
     const response = await fetch('http://localhost:8000'); // get temp session token from server.js (backend)
     const data = await response.json();
@@ -56,21 +57,30 @@ const run = async () => {
       texts[res.audio_start] = res.text;
       const keys = Object.keys(texts);
       keys.sort((a, b) => a - b);
+      console.log(keys);
       for (const key of keys) {
         if (texts[key]) {
           msg += ` ${texts[key]}`;
         }
       }
-      if(msg){
-        msg = msg.match(/[^_\W]+/g).join(' ');
-        messageEl.innerText = msg;
-      }
-    };
 
-    socket.onerror = (event) => {
-      console.error(event);
-      socket.close();
-    }
+      if(msg){
+        let final="";
+        msg = msg.match(/[^_\W]+/g).join(' ');
+        let split_msg = msg.split(" ");
+        for(let i=0; i<split_msg.length;i++){
+          if(chosen_words.includes(split_msg[i].toUpperCase())){
+            final += split_msg[i] + " ";
+          }else{
+            final += '<i>'+split_msg[i] + '</i> ';
+          }
+        }
+        messageEl.innerHTML = final;
+      }
+
+
+
+    };
     
     socket.onclose = event => {
       console.log("SOCKET CLOSED");
@@ -88,9 +98,12 @@ const run = async () => {
 
         var ran = Math.floor(Math.random() * lists[list_num].length);
 
-        sen_box.innerHTML += (lists[list_num][ran] + " ");
-      }
+        let word = lists[list_num][ran];
+        chosen_words.push(word.toUpperCase());
 
+        sen_box.innerHTML += (word.toUpperCase() + " ");
+      }
+      caption_div.style.width = '100%';
       buttonEl.innerText="End Recording";
       // once socket is open, begin recording
       messageEl.style.display = '';
